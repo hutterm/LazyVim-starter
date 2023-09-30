06d15f2995c693f463569f62492ec61ced39f736
Markus Hütter <huetter@outlook.com>
2023-09-30 13:15:07 +0200

    original lazyvim csharp

--- lua/plugins/csharp.lua
+++ lua/plugins/csharp.lua
@@ -1,71 +1,68 @@
 return {
-  -- {
-  --   "williamboman/mason.nvim",
-    -- opts = function(_, opts)
-    --   if type(opts.ensure_installed) == "table" then
-    --     vim.list_extend(opts.ensure_installed, { "netcoredbg" })
-    --   end
-    -- end,
-  -- },
+
+  { "Hoffs/omnisharp-extended-lsp.nvim", lazy = true },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "c_sharp" })
+      end
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      table.insert(opts.sources, nls.builtins.formatting.csharpier)
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        cs = { "csharpier" },
+      },
+      formatters = {
+        csharpier = {
+          command = "dotnet-csharpier",
+          args = { "--write-stdout" },
+        },
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      table.insert(opts.ensure_installed, "csharpier")
+    end,
+  },
   {
     "neovim/nvim-lspconfig",
     opts = {
-      autoformat = false,
       servers = {
         omnisharp = {
+          handlers = {
+            ["textDocument/definition"] = function(...)
+              return require("omnisharp_extended").handler(...)
+            end,
+          },
           keys = {
-            { "<leader>;", "mqA;<Esc>`q", desc = "; at end of line" },
+            {
+              "gd",
+              function()
+                require("omnisharp_extended").telescope_lsp_definitions()
+              end,
+              desc = "Goto Definition",
+            },
           },
-        },
-        -- csharp_ls = {},
-      },
-    },
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    opts = {
-      ensure_installed = { "coreclr" },
-    },
-  },
-  -- generate c# xml doc
-  {
-    "danymat/neogen",
-    cmd = { "Neogen" },
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    opts = {
-      languages = {
-        cs = {
-          template = {
-            annotation_convention = "xmldoc",
-          },
+          enable_roslyn_analyzers = true,
+          organize_imports_on_format = true,
+          enable_import_completion = true,
         },
       },
     },
-  },
-  {
-    "mfussenegger/nvim-dap",
-    config = function(plugin, ...)
-      plugin._.super.config(...)
-      local dap = require("dap")
-      dap.adapters.netcoredbg = {
-        type = "executable",
-        command = "netcoredbg",
-        args = { "--interpreter=vscode" },
-      }
-      require("dap.ext.vscode").load_launchjs(nil, { coreclr = { "cs" } })
-    end,
-  },
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "Issafalcon/neotest-dotnet",
-    },
-    opts = function(_, opts)
-      opts.adapters = {
-        ["neotest-dotnet"] = {
-          discovery_root = "solution",
-        },
-      }
-    end,
   },
 }
