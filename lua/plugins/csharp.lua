e5a4b5a583608de905b0aca363a8e7b2922f2394
Markus Hütter <huetter@outlook.com>
2023-09-30 13:17:18 +0200

    changes to original csharp

--- lua/plugins/csharp.lua
+++ lua/plugins/csharp.lua
@@ -37,6 +37,7 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       table.insert(opts.ensure_installed, "csharpier")
+      -- table.insert(opts.ensure_installed, "netcoredbg")
     end,
   },
   {
@@ -57,6 +58,7 @@ return {
               end,
               desc = "Goto Definition",
             },
+            { "<leader>;", "mqA;<Esc>`q", desc = "; at end of line" },
           },
           enable_roslyn_analyzers = true,
           organize_imports_on_format = true,
@@ -64,5 +66,52 @@ return {
         },
       },
     },
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = {
+      ensure_installed = { "coreclr" },
+    },
+  },
+  -- generate c# xml doc
+  {
+    "danymat/neogen",
+    cmd = { "Neogen" },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = {
+      languages = {
+        cs = {
+          template = {
+            annotation_convention = "xmldoc",
+          },
+        },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function(plugin, ...)
+      plugin._.super.config(...)
+      local dap = require("dap")
+      dap.adapters.netcoredbg = {
+        type = "executable",
+        command = "netcoredbg",
+        args = { "--interpreter=vscode" },
+      }
+      require("dap.ext.vscode").load_launchjs(nil, { coreclr = { "cs" } })
+    end,
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "Issafalcon/neotest-dotnet",
+    },
+    opts = function(_, opts)
+      opts.adapters = {
+        ["neotest-dotnet"] = {
+          discovery_root = "solution",
+        },
+      }
+    end,
   },
 }
