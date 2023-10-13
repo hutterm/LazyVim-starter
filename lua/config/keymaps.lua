a3d35635e967ed6b37e1154c6a9b1cc2120cafc7
Markus Hütter <huetter@outlook.com>
2023-10-13 15:37:51 +0200

    paste with c-v in command mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Keymap for pasting from system clipboard in command mode
+vim.keymap.set({ "c", "i" }, "<c-v>", "<c-r>+", { noremap = true })
