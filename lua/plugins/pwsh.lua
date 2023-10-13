3476672cb96a074103e3e7bf1c60dfa07b0877ca
Markus Hütter <huetter@outlook.com>
2023-10-13 15:38:22 +0200

    proper powershell

---
+++ lua/plugins/pwsh.lua
@@ -0,0 +1,21 @@
+return {
+
+  -- -- {
+  -- --   "williamboman/mason.nvim",
+  -- --   opts = function(_, opts)
+  -- --     opts.ensure_installed = opts.ensure_installed or {}
+  -- --     table.insert(opts.ensure_installed, "csharpier")
+  -- --     -- table.insert(opts.ensure_installed, "netcoredbg")
+  -- --   end,
+  -- -- },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       powershell_es = {
+  --         bundle_path = "%LocalAppData%/nvim-data/nvim-data/mason/packages/powershell-editor-services",
+  --       },
+  --     },
+  --   },
+  -- },
+}
