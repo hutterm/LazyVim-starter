c95ce1d30ab374ace0fea842d4ae25eb9079d7c5
Markus Hütter <huetter@outlook.com>
2023-11-26 19:15:33 +0100

    overseer f5

---
+++ lua/plugins/overseer.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "stevearc/overseer.nvim",
+    keys = {
+      { "<F5>", "<cmd>OverseerRun<CR>", desc = "Run Overseer" },
+      { "<leader><F5>", "<cmd>OverseerToggle<CR>", desc = "Toggle Overseer" },
+    },
+    opts = {},
+  },
+}
