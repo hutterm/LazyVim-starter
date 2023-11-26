ac76701551c940670f2423e44efcea17bc69f3d8
Markus Hütter <huetter@outlook.com>
2023-11-26 19:15:56 +0100

    roslyn cs

--- lua/plugins/csharp.lua
+++ lua/plugins/csharp.lua
@@ -1,15 +1,15 @@
 return {
 
-  { "Decodetalkers/csharpls-extended-lsp.nvim", lazy = true },
-  -- { "Hoffs/omnisharp-extended-lsp.nvim", lazy = true },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "c_sharp", "xml" })
-      end
-    end,
-  },
+  -- { "Decodetalkers/csharpls-extended-lsp.nvim", lazy = true },
+  -- -- { "Hoffs/omnisharp-extended-lsp.nvim", lazy = true },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   opts = function(_, opts)
+  --     if type(opts.ensure_installed) == "table" then
+  --       vim.list_extend(opts.ensure_installed, { "c_sharp", "xml" })
+  --     end
+  --   end,
+  -- },
   {
     "nvimtools/none-ls.nvim",
     optional = true,
@@ -41,61 +41,102 @@ return {
       -- table.insert(opts.ensure_installed, "netcoredbg")
     end,
   },
+  -- {
+  --   "jmederosalvarado/roslyn.nvim",
+  --   config = function(_, opts)
+  --     local roslyn = require("roslyn")
+  --     vim.notify("roslyn setup 1 ")
+  --     roslyn.setup()
+  --   end,
+  -- },
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      "jmederosalvarado/roslyn.nvim",
+    },
     opts = {
       servers = {
-        csharp_ls = {
-          root_dir = function(fname)
-            local util = require("lspconfig/util")
+        roslyn = {},
+      },
+      setup = {
+        roslyn = function(_, opts)
+          -- vim.notify(" roslyn setup")
+          opts.roslyn_version = "4.9.0-2.23563.2" --"4.9.0-1.23530.4"
+          opts.dottnet_cmd = "dotnet"
+          opts.capabilities = vim.tbl_deep_extend("force", opts.capabilities, {
+            filetypes = { "cs", "razor" },
 
-            return util.root_pattern("*.sln")(fname)
-              or util.root_pattern("*.csproj")(fname)
-              or util.root_pattern("*.fsproj")(fname)
-              or util.root_pattern(".git")(fname)
-              or util.find_git_ancestor(fname)
-              or util.path.dirname(fname)
-          end,
-
-          keys = {
-            -- {
-            --   "gd",
-            --   function()
-            --     require("omnisharp_extended").telescope_lsp_definitions()
-            --   end,
-            --   desc = "Goto Definition",
+            --   didChangeWatchedFiles = {
+            --     dynamicRegistration = false,
+            --   },
             -- },
-            { "<leader>;", "mqA;<Esc>`q", desc = "; at end of line" },
-          },
-          --   handlers = {
-          --     ["textDocument/definition"] = function(...)
-          --       return require("omnisharp_extended").handler(...)
-          --     end,
-          --   },
-        },
-        -- omnisharp = {
-        --   handlers = {
-        --     ["textDocument/definition"] = function(...)
-        --       return require("omnisharp_extended").handler(...)
-        --     end,
-        --   },
-        --   keys = {
-        --     {
-        --       "gd",
-        --       function()
-        --         require("omnisharp_extended").telescope_lsp_definitions()
-        --       end,
-        --       desc = "Goto Definition",
-        --     },
-        --     { "<leader>;", "mqA;<Esc>`q", desc = "; at end of line" },
-        --   },
-        --   enable_roslyn_analyzers = true,
-        --   organize_imports_on_format = true,
-        --   enable_import_completion = true,
-        -- },
+          })
+          opts.on_attach = function(client, bufnr)
+            -- client.handlers["textDocument/definition"]
+          end
+          -- vim.notify(vim.inspect(opts))
+          require("roslyn").setup(opts)
+          -- vim.notify(" roslyn setup")
+          return true
+        end,
       },
     },
   },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       csharp_ls = {
+  --         root_dir = function(fname)
+  --           local util = require("lspconfig/util")
+  --
+  --           return util.root_pattern("*.sln")(fname)
+  --             or util.root_pattern("*.csproj")(fname)
+  --             or util.root_pattern("*.fsproj")(fname)
+  --             or util.root_pattern(".git")(fname)
+  --             or util.find_git_ancestor(fname)
+  --             or util.path.dirname(fname)
+  --         end,
+  --
+  --         keys = {
+  --           -- {
+  --           --   "gd",
+  --           --   function()
+  --           --     require("omnisharp_extended").telescope_lsp_definitions()
+  --           --   end,
+  --           --   desc = "Goto Definition",
+  --           -- },
+  --           { "<leader>;", "mqA;<Esc>`q", desc = "; at end of line" },
+  --         },
+  --         --   handlers = {
+  --         --     ["textDocument/definition"] = function(...)
+  --         --       return require("omnisharp_extended").handler(...)
+  --         --     end,
+  --         --   },
+  --       },
+  --       -- omnisharp = {
+  --       --   handlers = {
+  --       --     ["textDocument/definition"] = function(...)
+  --       --       return require("omnisharp_extended").handler(...)
+  --       --     end,
+  --       --   },
+  --       --   keys = {
+  --       --     {
+  --       --       "gd",
+  --       --       function()
+  --       --         require("omnisharp_extended").telescope_lsp_definitions()
+  --       --       end,
+  --       --       desc = "Goto Definition",
+  --       --     },
+  --       --     { "<leader>;", "mqA;<Esc>`q", desc = "; at end of line" },
+  --       --   },
+  --       --   enable_roslyn_analyzers = true,
+  --       --   organize_imports_on_format = true,
+  --       --   enable_import_completion = true,
+  --       -- },
+  --     },
+  --   },
+  -- },
   {
     "jay-babu/mason-nvim-dap.nvim",
     opts = {
